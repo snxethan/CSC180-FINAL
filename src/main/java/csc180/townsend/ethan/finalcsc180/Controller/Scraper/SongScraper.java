817b73ad420e9161bc3fb1d766d75e12b4de81d1
Tomonator1000 <tommysmith12443@gmail.com>
2024-08-27 15:12:59 -0600

    Added Request Method

--- src/main/java/csc180/townsend/ethan/finalcsc180/Controller/Scraper/SongScraper.java
+++ src/main/java/csc180/townsend/ethan/finalcsc180/Controller/Scraper/SongScraper.java
@@ -1,5 +1,10 @@
 package csc180.townsend.ethan.finalcsc180.Controller.Scraper;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -11,7 +16,20 @@ import java.util.List;
 
 public class SongScraper {
 
-    private static final String URL = "https://kworb.net/spotify/country/global_weekly.html";
+    private static final String URL = "https://kworb.net/spotify/listeners.html";
+
+    public static String request(){
+        //starts fetching info from URL
+        HttpGet request = new HttpGet(URL);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(request)) {
+            String responseBody = EntityUtils.toString(response.getEntity());
+            return responseBody;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 //
 //    public static List<String> getTop200Songs() throws IOException {
 //        List<String> topSongs = new ArrayList<>();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module csc180.townsend.ethan.finalcsc180 {
     requires javafx.fxml;
     requires java.sql;
     requires org.jsoup;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.apache.httpcomponents.httpclient;
 
     opens csc180.townsend.ethan.finalcsc180.Controller to javafx.fxml;
     exports csc180.townsend.ethan.finalcsc180;
